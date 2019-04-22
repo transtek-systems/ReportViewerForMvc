5e238fac126c3b80c4e76b2de9574dc51c06d8d3
Mihear.Al <Mihear.Al@Rami-PC>
2019-04-22 15:54:30 +0300

    Fix bug of static report viewer

--- ReportViewerForMvc/ReportViewerWebForm.aspx.cs
+++ ReportViewerForMvc/ReportViewerWebForm.aspx.cs
@@ -9,19 +9,32 @@ namespace ReportViewerForMvc
     /// </summary>
     public partial class ReportViewerWebForm : System.Web.UI.Page
     {
+        private string DynamicID;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             BuildReportViewer();
+
+            // register eventhandler to remove the reportviewer
+            ReportViewer1.Disposed += ReportViewerDisposingEventHandler;
         }
 
         private void BuildReportViewer()
         {
             if (!IsPostBack)
             {
-                ReportViewerForMvc.ReportViewer.ID = ReportViewer1.ID;
+                DynamicID = Request.QueryString["DynamicID"];
+                ReportViewerForMvc.ReportViewer(DynamicID).ID = ReportViewer1.ID;
 
-                ReportViewer1.SetProperties(ReportViewerForMvc.ReportViewer);
+                ReportViewer1.SetProperties(ReportViewerForMvc.ReportViewer(DynamicID));
             }
+        }
+
+        protected void ReportViewerDisposingEventHandler(object sender, EventArgs e)
+        {
+            var reportViewer = ReportViewerForMvc.Remove(DynamicID);
+            if (reportViewer != null)
+                reportViewer.Dispose();
         }
     }
 }
